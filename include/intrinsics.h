25b1bb0f9f3b3bd170ceee7f0f57e90f65fa2a9e
cheetah671 <arnavagnihotri507@gmail.com>
2025-08-21 14:00:00 +0530

    declare builtin command entry points

---
+++ include/intrinsics.h
@@ -0,0 +1,12 @@
+// include/intrinsics.h
+#ifndef INTRINSICS_H
+#define INTRINSICS_H
+
+#include "shell.h"
+
+void activities();
+void ping(char* args);
+void fg(char* args);
+void bg(char* args);
+
+#endif
