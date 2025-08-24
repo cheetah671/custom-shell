de3bf0caee1dd9b3281956544e0f92c3eeb7e3d9
cheetah671 <arnavagnihotri507@gmail.com>
2025-08-24 10:00:00 +0530

    add activities builtin

---
+++ src/activities.c
@@ -0,0 +1,11 @@
+#include "activities.h"
+#include "jobs.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+
+void activities() {
+    print_jobs();
+}
